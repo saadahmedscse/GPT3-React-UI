b2d83fa6c7fa521150e5ff701ecc3f0f28951dab
saadahmedscse <saadahmedscse@gmail.com>
2023-09-01 18:54:43 +0600

    created project structure

---
+++ src/components/cta/ClickToAction.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './clickToAction.css'
+
+const ClickToAction = () => {
+    return (
+        <div>
+            Click to action
+        </div>
+    )
+}
+
+export default ClickToAction
